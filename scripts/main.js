52aedd432f7bd9498c21aa9fa86248a587f3eff7
MariaPaulaAlcantara <mariapaula.contato1@gmail.com>
2022-06-27 22:05:30 -0300

    salvando as informacoes no localStorage (quando a pagina é atualizada as informacoes somem da tela)

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,31 @@
 const form = document.querySelector("#formulario");
+const dados = JSON.parse(localStorage.getItem("dadosProduto")) || [];
 
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault(); //previnindo o comportamento padrão desse elemento
     console.log("clicado");
     console.log(evento);
 
-    const nomeDoFilme = evento.target.elements['nomeDoFilme'].value;
-    const nomeGeneroDoFilme = evento.target.elements['generoDoFilme'].value;
-    const nomePlataformaDoFilme = evento.target.elements['plataformaDoFilme'].value;
+    const nomeDoFilme = evento.target.elements['nomeDoFilme'];
+    const nomeGeneroDoFilme = evento.target.elements['generoDoFilme'];
+    const nomePlataformaDoFilme = evento.target.elements['plataformaDoFilme'];
 
+    const registroDosFilmes = {
+        nomeDoFilme: nomeDoFilme.value,
+        generoDoFilme: nomeGeneroDoFilme.value,
+        plataformaDoFilme: nomePlataformaDoFilme.value
+    }
 
-    console.log(nomeDoFilme);
-    console.log(nomeGeneroDoFilme);
-    console.log(nomePlataformaDoFilme);
+    dados.push(registroDosFilmes);
 
-//Limpar o campo
-    // nomeDoFilme.value = "";
-    // nomeGeneroDoFilme.value = "";
-    // nomePlataformaDoFilme.value = "";
+    localStorage.setItem("dadosProduto", JSON.stringify(dados));
 
-    criarElemento(nomeDoFilme, nomeGeneroDoFilme, nomePlataformaDoFilme);
+    criarElemento(nomeDoFilme.value, nomeGeneroDoFilme.value, nomePlataformaDoFilme.value);
+
+    //Limpar o campo
+    nomeDoFilme.value = "";
+    nomeGeneroDoFilme.value = "";
+    nomePlataformaDoFilme.value = "";
 
 
 });
@@ -42,3 +48,14 @@ function criarElemento(nomeDoFilme, nomeGeneroDoFilme, nomePlataformaDoFilme){
 
 
 
+// localStorage
+// A estrutura de storage de dados é bem simples, composta pelo par chave e valor. Um exemplo:
+// {
+//     chave: valor;
+// }
+// os dados que serão gravados ali precisam ser convertidos para strings antes de serem gravados. Para fazer isso, basta usar o método JSON.stringify() antes de passar o valor para o setItem().
+
+// localStorage.setItem() para criar um novo par de chave: valor;
+// localStorage.getItem() para recuperar o valor do par chave: valor;
+// localStorage.removeItem() para remover um par específico;
+// localStorage.clear() apaga TODOS os pares gravados pro aquele domínio;
